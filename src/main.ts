9a9f0c7c702d923798998c4db085396954f18115
MrKrrot <rafita.olguin@hotmail.com>
2022-11-15 17:20:32 -0600

    The API is now in the index route

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import 'dotenv/config'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    await app.listen(5000)
+    app.useGlobalPipes(new ValidationPipe())
+
+    const config = new DocumentBuilder()
+        .setTitle('SkyHome API')
+        .setDescription('The Official SkyHome API')
+        .setVersion('1.0')
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config)
+
+    SwaggerModule.setup('/', app, document)
+    await app.listen(process.env.PORT || 5000)
 }
 bootstrap()
