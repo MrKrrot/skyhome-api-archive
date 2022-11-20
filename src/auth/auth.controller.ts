f81ed7de34912788d0bf3ef8042c72e93485abff
MrKrrot <rafita.olguin@hotmail.com>
2022-11-19 22:16:48 -0600

    Fix Auth name tag

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,7 @@ import { CreateUserDto } from 'src/users/dto/create-user.dto'
 import { AuthService } from './auth.service'
 import { LoginDto } from './dto/login.dto'
 
-@ApiTags('auth')
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
