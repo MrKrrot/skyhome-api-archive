935fd60237906e0723965f1fc5d3b051f27b05af
MrKrrot <rafita.olguin@hotmail.com>
2022-12-21 21:45:04 -0600

    Added dummy methods in files services

--- src/fm/files/files.controller.ts
+++ src/fm/files/files.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Delete, HttpException, HttpStatus, Patch, Post } from '@nestjs/common'
+import { UseGuards } from '@nestjs/common/decorators'
+import { ApiBearerAuth, ApiHeader, ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/jwt.guard';
+import { FilesService } from './files.service';
 
+@ApiTags('Files')
+@ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer token',
+})
+@ApiBearerAuth()
+@UseGuards(JwtGuard)
 @Controller('files')
-export class FilesController {}
+export class FilesController {
+    constructor(private readonly filesService: FilesService) {}
+
+    @Post()
+    upload() {
+        return this.filesService.upload()
+    }
+
+    @Post(':folderId')
+    uploadOnFolder() {
+        return this.filesService.uploadOnFolder()
+    }
+
+    @Patch(':fileName')
+    rename() {
+        return this.filesService.rename()
+    }
+
+    @Patch(':folderId/:fileName')
+    renameOnFolder() {
+        return this.filesService.renameOnFolder()
+    }
+
+    @Delete(':fileName')
+    delete() {
+        return this.filesService.delete()
+    }
+
+    @Delete(':folderId/:fileName')
+    deleteOnFolder() {
+        return this.filesService.deleteOnFolder()
+    }
+}
--- src/fm/files/files.service.ts
+++ src/fm/files/files.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+    constructor() { }
+
+    upload() {
+        return 'This action uploads a file'
+    }
+
+    uploadOnFolder() {
+        return 'This action uploads a file on a folder'
+    }
+
+    rename() {
+        return 'This action renames a file'
+    }
+
+    renameOnFolder() {
+        return 'This action renames a file on a folder'
+    }
+
+    delete() {
+        return 'This action deletes a file'
+    }
+
+    deleteOnFolder() {
+        return 'This action deletes a file on a folder'
+    }
+}
